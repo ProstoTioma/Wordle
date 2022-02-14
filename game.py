82c96fd5db98f389dd2a90124ed03ce58d67988b
ProstoTioma <artemyermolenko14@gmail.com>
2022-02-14 15:29:08 +0100

    word guessing and bug fixes

--- game.py
+++ game.py
@@ -6,10 +6,12 @@ import random
 class Game:
     def __init__(self):
         self.screen = Screen()
-        self.words_list = ['Гиена', 'Петух']
+        self.words_list = ['клоун']
         self.word = random.choice(self.words_list)
         self.alphabet = 'йцукенгшщзхъфывапролджэячсмитьбю'
         self.index = 0
+
+        self.row_ind = 1
 
         self.letters = list()
 
@@ -34,12 +36,12 @@ class Game:
                             except ValueError:
                                 pass
                     self.letters = removed_blanks
-                    if len(self.letters) < 5:
+                    if len(self.letters) < 5 * self.row_ind:
                         if event.unicode in self.alphabet and event.unicode is not '':
                             field = self.screen.field_squares
                             if self.index < len(field):
                                 sq = field[self.index]
-                                font = pygame.font.Font('futur.ttf', int(self.screen.sq_size // 2))
+                                font = pygame.font.Font('futur.ttf', int(self.screen.sq_size // 3))
                                 letter = font.render(event.unicode.upper(), True, (255, 255, 255),
                                                      self.screen.bg)
                                 letter_rect = letter.get_rect()
@@ -47,29 +49,35 @@ class Game:
 
                                 self.letters.append([event.unicode.upper(), letter_rect])
                                 self.index += 1
-                                print(self.letters[self.index - 1][0])
+                                # print(self.letters[self.index - 1][0])
 
                     if event.key == pygame.K_BACKSPACE:
-                        if self.index > 0:
+                        if self.index > 0 and self.index > (5 * (self.row_ind - 1)):
                             self.letters[self.index - 1][0] = '    '
                             self.index -= 1
-                            print(self.letters[self.index - 1][0])
+                            # print(self.letters[self.index - 1][0])
 
-                    if event.key == pygame.K_RETURN:
+                    if event.key == pygame.K_RETURN and len(self.letters) == 5 * self.row_ind:
                         self.check_word()
+                        self.row_ind += 1
+                        # self.letters.clear()
 
             pygame.display.flip()
 
     def blit_letters(self):
         for letter in self.letters:
             letter_rect = letter[1]
-            font = pygame.font.Font('futur.ttf', int(self.screen.sq_size // 2))
+            font = pygame.font.Font('futur.ttf', int(self.screen.sq_size // 3))
             let = font.render(letter[0], True, (255, 255, 255), self.screen.bg)
             self.screen.screen.blit(let, letter_rect)
 
     def check_word(self):
-        for i in range(len(self.letters)):
-            if self.letters[i] == self.word[i].upper():
-                rect = self.screen.field_squares[i]
+        for i in range(5):
+            if self.letters[i + 5 * (self.row_ind - 1)][0] == self.word[i].upper():
+                rect = self.screen.field_squares[i + 5 * (self.row_ind - 1)]
                 pygame.draw.rect(self.screen.screen, (0, 255, 0), rect, 1)
+                pygame.display.flip()
+            elif self.letters[i + 5 * (self.row_ind - 1)][0] in self.word.upper():
+                rect = self.screen.field_squares[i + 5 * (self.row_ind - 1)]
+                pygame.draw.rect(self.screen.screen, (220, 197, 31), rect, 1)
                 pygame.display.flip()
